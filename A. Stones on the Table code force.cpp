05cc93f29dacb7f80dc54795ba71585b84124058
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-07-26 21:07:11 +0600

    Add files via upload

---
+++ A. Stones on the Table code force.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int k,n,w,i,d,f;
+    cin>>k>>n>>w;
+    for(i=1;i<=w;i++)
+    {
+       d=d+i*k;
+
+    }
+f=d-n;
+if(f>0)
+cout<<f;
+else
+{
+    cout<<"0";
+}
+}
