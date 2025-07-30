5f9df53ef49a8d0c554ff47e793fca6d94a87524
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-07-31 00:21:03 +0600

    Create A.System of Equation.cpp

---
+++ A.System of Equation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main()
+{
+    int n;
+    cin>>n;
+    int ans=n/100;
+    int left=n%100;
+
+    ans+=left/20;
+    left=left%20;
+
+    ans+=left/10;
+    left=left%10;
+
+    ans+=left/5;
+    left=left%5;
+
+    ans+=left/1;
+    left=left%1;
+
+    cout<<ans<<endl;
+    return 0;
+}
