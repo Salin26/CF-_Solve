3dab56b6b294d31dab74fee7242c6b27c80b1637
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-07-30 16:06:09 +0600

    Create A.Minimize!.cpp

---
+++ A.minimize!.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b;
+        c=b-a;
+        cout<<c;
+    }
+}
