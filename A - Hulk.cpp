2e34856bb78b579d7a7f9e51ee7aa6572f347f55
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-07-31 20:24:12 +0600

    Create A - Hulk.cpp

---
+++ A - Hulk.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        if(i==1)
+        {
+            cout<<"I hate ";
+        }
+        else if(i%2!=0)
+        {
+            cout<<" that I hate ";
+        }
+        else
+        {
+            cout<<" that I love ";
+        }
+
+
+    }
+    cout<<" it";
+    return 0;
+}
