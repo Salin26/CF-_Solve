c1b2c6390cfc729dee1597422ad53e6d40755609
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-11-11 12:18:31 +0600

    Implement string comparison in B.Your_name.cpp

---
+++ B.Your_name.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    
+    string s,k;
+    
+    cin>>s>>k;
+   
+    sort(s.begin(), s.end());
+    sort(k.begin(), k.end());
+    
+    if(s==k)
+    {
+      cout<<"yes"<<endl;
+    }
+    else
+    {
+      cout<<"NO"<<endl;
+    } 
+    
+  }
+}
