d4c5895b3783512dfcc677a14cd4df984d7adbaf
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-11-16 15:06:38 +0600

    Implement square drawing logic in A - Draw a Square.cpp

---
+++ A - Draw a Square.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  
+  while(t--)
+  {
+    int l,r,d,u;
+    cin>>l>>r>>d>>u;
+    
+    if(l==r&&l==d&&l==u&&r==d&&r==u&&d==u)
+    {
+      cout<<"YES"<<endl;
+    }
+    else
+    {
+      cout<<"NO"<<endl;
+    }
+   
+  }
+  return 0;
+}
