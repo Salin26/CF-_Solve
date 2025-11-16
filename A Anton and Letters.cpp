3013ae87e6ed1b29226ea35865ec1ac6ff416fb8
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-11-16 15:08:03 +0600

    Create A Anton and Letters.cpp

---
+++ A Anton and Letters.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    set<char> st;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            st.insert(s[i]);
+        }
+    }
+
+    cout << st.size() << endl;
+    return 0;
+}
