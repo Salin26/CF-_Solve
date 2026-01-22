367060ea52c0bb2a0cdf932f68b1c67070b7a43c
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2026-01-22 14:48:02 +0600

    Add main function for social experiment logic

---
+++ A - Social Experiment..cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+        if(n>3)
+        {
+            cout<<n%2<<endl;
+        }
+        
+       else
+       {
+         cout<<n<<endl;
+       }
+        
+    
+}
+return 0;
+}
+//salin is back
