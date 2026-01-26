82cacaff8814cfab4a16f60ba9131e2ba50fd551
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2026-01-26 21:54:03 +0600

    Create A - Consecutive Sum Riddle.cpp

---
+++ A - Consecutive Sum Riddle.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define yes           cout << "YES" << endl;
+#define no            cout << "NO" << endl;
+#define ll         long long
+using namespace std;
+
+int main() 
+{
+  
+   
+    ll t;
+    cin>>t;
+     while(t--)
+     {
+      ll n;
+      cin>>n;
+     cout<< -(n-1)<<" "<<n<<endl;
+     }
+
+return 0;
+}
+
+      
+      
+    
+      
+  
+
