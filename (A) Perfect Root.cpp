b698504b32cdbe6403e7f1e9ec0070077e914a6d
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2026-01-26 21:53:35 +0600

    Create (A) Perfect Root.cpp

---
+++ (A) Perfect Root.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define yes           cout << "YES" << endl;
+#define no            cout << "NO" << endl;
+#define ll         long long
+using namespace std;
+
+int main() 
+{
+  
+   
+    ll t;
+    cin>>t;
+     while(t--)
+     {
+      ll n;
+      cin>>n;
+      for(int i=1;i<=n;i++)cout<<i<<" ";
+      cout<<endl;
+     }
+
+return 0;
+}
+
+      
+      
+    
+      
+  
+
